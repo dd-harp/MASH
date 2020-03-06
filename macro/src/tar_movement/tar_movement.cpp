ceb070753b1e1696bc918e02ae8b2c9178b32683
slwu89 <slwu89@berkeley.edu>
2020-03-05 16:36:48 -0800

    nothing of significance

--- macro/src/tar_movement/tar_movement.cpp
+++ macro/src/tar_movement/tar_movement.cpp
@@ -8,3 +8,16 @@
 
 
 #include "tar_movement.h"
+
+
+/* --------------------------------------------------------------------------------
+#   class for the TaR movement
+-------------------------------------------------------------------------------- */
+
+// indd_harp::tar_movement movement object
+void dd_harp::tar_movement::init(
+  const std::map<std::string, tar_movement_parameter> &parameters,
+  const std::vector<std::vector<int> > &initial_location
+){
+
+};
