de3f19cd488032d7d0f5f1a6db3be7494db97d62
Drew Dolgert <adolgert@uw.edu>
2020-04-16 16:52:58 -0700

    adding a continuous time implementation that passes tests

--- macro/R/continuous_step.R
+++ macro/R/continuous_step.R
@@ -1,6 +1,11 @@
+#' Set up firing times given an initial state.
+#'
+#' @param individuals a data frame of individuals, including columns for times.
+#' @param is_enabled a list of functions that say when transitions are enabled
+#' @param when a list of functions that return times when enabled
+#' @return a data frame of individuals, with times filled in
+#' @export
 initialize_times <- function(individuals, is_enabled, when) {
-  when_col <- ncol(individuals) - length(when)
-  times_end_col <- ncol(individuals)
   for (person_idx in 1:nrow(individuals)) {
     state <- as.list(individuals[person_idx])
     newly_enabled <- vapply(
@@ -11,41 +16,93 @@ initialize_times <- function(individuals, is_enabled, when) {
       when[newly_enabled],
       FUN = function(x) x(state),
       FUN.VALUE = vector(mode = "numeric", length = 1))
-    individuals[person_idx, when_col, times_end_col] <- c(min(new_when), new_when)
+    state[names(new_when)] <- new_when
+    state["when"] <- min(new_when)
+    individuals[person_idx, ] <- state
   }
   individuals
 }
 
+#' Update one individual
+#'
+#' @param state a list of their traits and times
+#' @param is_enabled a list of functions to determine enabling
+#' @param when a list of functions to determine when to fire
+#' @param fire a list of functions to determine firing
+#' @return a list with the individual a trajectory entry, and a new time
+#' @export
+update_individual <- function(state, is_enabled, when, fire) {
+  current_time <- state$when
+  # Select the transition times (excluding the minimum time "when")
+  # We assume that the lists of functions are all in the same order.
+  when_col <- length(state) - length(fire)
+  times <- unlist(state[(when_col + 1):length(state)])
 
+  # Fire!
+  to_fire <- which.min(times)
+  trajectory <- list(event = names(to_fire), when = current_time)
+  new_state <- fire[[to_fire]](state, current_time)
+  times[to_fire] <- Inf  # mark the one that fired as not being enabled so it can fire next.
+
+  # Turn transitions on/off as a result of firing.
+  was_enabled <- is.finite(times)
+  enabled <- vapply(
+    is_enabled,
+    FUN = function(x) x(new_state),
+    FUN.VALUE = vector(mode = "logical", length = 1))
+  times[!enabled] <- Inf  # disable competing transitions
+
+  # Compute times for newly-enabled transitions
+  newly_enabled <- !was_enabled & enabled
+  if (any(newly_enabled)) {
+    new_times <- current_time +
+      vapply(
+        when[newly_enabled],
+        FUN = function(x) x(new_state),
+        FUN.VALUE = vector(mode = "numeric", length = 1))
+    times[newly_enabled] <- new_times
+  }
+  new_state[when_col:length(new_state)] <- c(min(times), times)
+  list(individual = new_state, trajectory = trajectory, time = current_time)
+}
+
+
+#' Check preconditions for the data structures.
+#' @param individuals a data frame of individuals, including columns for times.
+#' @param is_enabled a list of enabling functions.
+#' @param when a list of functions that return firing times when enabled
+#' @param fire a list of functions that say when to fire.
+check_continuous_setup <- function(individuals, is_enabled, when, fire) {
+  stopifnot(all(names(is_enabled) == names(when)))
+  stopifnot(all(names(is_enabled) == names(fire)))
+  when_col <- length(individuals) - length(is_enabled)
+  stopifnot(names(individuals)[when_col] == "when")
+  stopifnot(all(names(individuals)[(when_col + 1):length(individuals)] == names(is_enabled)))
+}
+
+
+#' Run until a specified time.
+#'
+#' @param individuals a data frame of individuals, including columns for times.
+#' @param is_enabled a list of enabling functions.
+#' @param when a list of functions that return firing times when enabled
+#' @param fire a list of functions that say when to fire.
+#' @return a trajectory
+#' @export
 continuous_step <- function(individuals, is_enabled, when, fire) {
+  check_continuous_setup(individuals, is_enabled, when, fire)
   # The current time and next firing times are part of the next state of the system.
   # If a firing time is Inf, that means it isn't scheduled.
-  current_time <- 0
   individuals <- initialize_times(individuals, is_enabled, when)
 
   step_cnt <- 5
   trajectory <- vector(mode = "list", length = step_cnt)
   for (step_idx in 1:step_cnt) {
-    next_person <- as.list(individuals[order(when)][1])
-    current_time <- next_person$when
-    if (is.infinite(current_time)) break
-    times <- next_person[4:length(next_person)]
-    to_fire <- which.min(times)
-    trajectory[[step_idx]] <- list(event = names(to_fire), when = current_time)
-    individual <- fire[[to_fire]](individual, current_time)
-    was_enabled <- is.finite(times)
-    newly_enabled <- vapply(
-      is_enabled,
-      FUN = function(x) x(individual),
-      FUN.VALUE = vector(mode = "logical", length = 1))
-    times[was_enabled & !newly_enabled] <- Inf
-    newly_enabled <- when[!was_enabled & newly_enabled]
-    new_times <- current_time +
-      vapply(
-        newly_enabled,
-        FUN = function(x) x(individual),
-        FUN.VALUE = vector(mode = "numeric", length = 1))
-    individuals[order(when)][1][3:ncol(individuals)] <- c(min(new_times), new_times)
+    individual <- as.list(individuals[order(when)][1])
+    if (is.infinite(individual$when)) break
+    new_state <- update_individual(individual, is_enabled, when, fire)
+    trajectory[[step_idx]] <- new_state$trajectory
+    individuals[order(when)][1] <- new_state$individual
   }
   do.call(rbind, trajectory)
 }
--- macro/tests/testthat/test-continuous_step.R
+++ macro/tests/testthat/test-continuous_step.R
@@ -1,4 +1,83 @@
+test_that("individual two state bounce", {
+  set.seed(32432)
+  is_enabled <- list(
+    infect = function(state) with(state, disease == "S"),
+    recover = function(state) with(state, disease == "I")
+  )
+  when <- list(
+    infect = function(state) rexp(1, 1/50),
+    recover = function(state) rexp(1, 1/200)
+  )
+  fire <- list(
+    infect = function(state, time) {within(state, {disease = "I"})},
+    recover = function(state, time) {within(state, {disease = "S"})}
+  )
+
+  individual <- list(disease = "S", when = 0.32, infect = 0.32, recover = Inf)
+  status <- update_individual(individual, is_enabled, when, fire)$individual
+  expect_equal(status$disease, "I")
+  expect_equal(status$infect, Inf)
+  expect_gt(status$recover, 0.32)
+  expect_equal(status$when, status$recover)
+
+  individual <- list(disease = "I", when = 0.44, infect = Inf, recover = 0.44)
+  status <- update_individual(individual, is_enabled, when, fire)$individual
+  expect_equal(status$disease, "S")
+  expect_equal(status$recover, Inf)
+  expect_gt(status$infect, 0.44)
+  expect_equal(status$when, status$infect)
+})
+
+
+#' The individual is in an absorbing state, so there is nothing
+#' else to fire. Check that it goes to Inf for the next time.
+test_that("individual removed goes to inf", {
+  set.seed(32432)
+  is_enabled <- list(
+    infect = function(state) with(state, disease == "S")
+  )
+  when <- list(
+    infect = function(state) rexp(1, 1/50)
+  )
+  fire <- list(
+    infect = function(state, time) {within(state, {disease = "I"})}
+  )
+
+  individual <- list(disease = "S", when = 0.32, infect = 0.32)
+  # browser()
+  status <- update_individual(individual, is_enabled, when, fire)$individual
+  expect_equal(status$disease, "I")
+  expect_equal(status$infect, Inf)
+  expect_equal(status$when, Inf)
+})
+
+
+#' A transition was enabled and fired, but it's now enabled again
+#' in the new state. Check that it gets scheduled again.
+test_that("individual transition fires again", {
+  set.seed(32432)
+  is_enabled <- list(
+    infect = function(state) with(state, disease == "S")
+  )
+  when <- list(
+    infect = function(state) rexp(1, 1/50)
+  )
+  # Returns to same state when it fires.
+  fire <- list(
+    infect = function(state, time) {within(state, {disease = "S"})}
+  )
+
+  individual <- list(disease = "S", when = 0.32, infect = 0.32)
+  # browser()
+  status <- update_individual(individual, is_enabled, when, fire)$individual
+  expect_equal(status$disease, "S")
+  expect_gt(status$infect, 0.32)
+  expect_equal(status$when, status$infect)
+})
+
+
 test_that("can use data.table", {
+  set.seed(97234)
   # This sets up the transitions.
   program_globals <- new.env()
   program_globals$b <- 0.2
@@ -35,7 +114,13 @@ test_that("can use data.table", {
     infect = rep(Inf, 3),
     recover = rep(Inf, 3)
   )
+  # Setting the key makes it much faster to find the soonest time.
   data.table::setkey(individuals, when)
 
+  # Test initialization.
+  initialized <- initialize_times(individuals, is_enabled, when)
+  # Check that, for this model, each individual has at least one transition initialized.
+  expect_true(all(is.finite(initialized[, infect]) | is.finite(initialized[, recover])))
   trajectory <- continuous_step(individuals, is_enabled, when, fire)
+  expect_equal(length(trajectory), 2 * 5)
 })
