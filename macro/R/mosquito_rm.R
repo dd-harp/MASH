4ab6405c384e115b84f1277d27e01b725aa627b5
Drew Dolgert <adolgert@uw.edu>
2020-09-18 15:25:12 -0700

    made the ten-day time step inside the mosquito module

--- macro/R/mosquito_rm.R
+++ macro/R/mosquito_rm.R
@@ -190,10 +190,23 @@ mosquito_rm_dynamics <- function(state, parameters, kappa, aquatic = mosquito_rm
 }
 
 
+#' Convert the input bloodmeals into an internal input.
+#' @param bloodmeal_dt A data table of bloodmeal inputs
+#' @return A vector of kappa for the mosquito_rm model.
+mosquito_rm_convert_bloodmeal <- function(bloodmeal_dt) {
+  numeric(5)
+}
+
+
+#' Create a Mosquito_rm module for Ross-Macdonald mosquitoes
+#' @param parameters A block of parameters for this model.
+#' @return A new module.
+#' @export
 mosquito_rm_module <- function(parameters) {
   infectious <- rep(0.4, parameters$N)
   mortality <- 1 - parameters$p
   module <- list(
+    external_parameters = parameters,
     parameters = build_internal_parameters(parameters),
     state = build_biting_state(infectious, mortality, parameters$maxEIP)
     )
@@ -202,24 +215,96 @@ mosquito_rm_module <- function(parameters) {
 }
 
 
-mash_step.mosquito_rm <- function(module, bloodmeal_dt) {
+mosquito_rm_aggregate_state <- function(output, state) {
+  output
+}
+
+
+#' An average of kappa over the past ten days so that we can project forward.
+#' @param kappa A matrix of values for each patch, for all days. Nx10.
+#' @return A matrix of kappa values for each patch and day, Nx10.
+#'
+#' We use this function because it can look at trends instead of a
+#' simple average. This version weights most recent days to construct
+#' a constant value for the future.
+#'
+#' @export
+mosquito_rm_average_kappa <- function(kappa) {
+  duration <- dim(kappa)[2]
+  weights <- exp((-1 / duration) * (duration - 1:duration))
+  weights <- weights / sum(weights)
+  one_day <- rowSums(kappa) %*% weights
+  as_row <- rep(one_day, duration)
+  matrix(as_row, ncol = duration)
+}
+
+
+#' A trend of kappa over the past ten days so that we can project forward.
+#' @param kappa A matrix of values for each patch, for all days. Nx10.
+#' @return A matrix of kappa values for each patch and day, Nx10.
+#'
+#' We use this function because it can look at trends instead of a
+#' simple average. This version uses an autoregressive process to
+#' find a trend.
+#'
+#' @export
+mosquito_rm_trended_kappa <- function(kappa) {
+  N <- dim(kappa)[1]
+  duration <- dim(kappa)[2]
+  # Start with matrix rotated so that it has stride-1 access.
+  result <- matrix(nrow = duration, ncol = duration)
+  for (patch in 1:N) {
+    result[, patch] <- as.vector(predict(
+      ar.ols(
+        ts(xx, start = 0, end = duration - 1)
+        ),
+      n.ahead = duration,
+      se = FALSE
+      ))
+  }
+  t(duration)
+}
+
+
+#' One discrete time step for the mosquito_rm module.
+#' @param module The mosquito_rm module.
+#' @param past_kappa Infectiousness to mosquitoes over discrete-time step.
+#' @return Observation of the time step and new state as a list
+#'     with `future_state` and `output`.
+#' @export
+mosquito_rm_discrete_step <- function(module, past_kappa) {
   params <- module$parameters
-  past_kappa <- mosquito_rm_convert_bloodmeal(bloodmeal_dt)
   today_state <- module$state
   for (i in 1:10) {
     today_state <- mosquito_rm_dynamics(today_state, params, past_kappa)
   }
   future_kappa <- mosquito_rm_average_kappa(past_kappa)
   future_state <- mosquito_rm_copy_state(today_state)
-  output <- NULL
+  output <- list()
   for (i in 1:10) {
     future_state <- mosquito_rm_dynamics(future_state, params, future_kappa)
-    output <- aggregate_state(output, future_state)
+    output[[i]] <- future_state$Z
   }
+  list(
+    future_state = future_state,
+    output = do.call(cbind, output)
+  )
+}
+
+
+#' One discrete time step for the mosquito_rm module.
+#' @param module The mosquito_rm module.
+#' @param bloodmeal_dt The input bloodmeal data from the last time step.
+#' @return The mosqutio_rm module back again, after the time step.
+#' @export
+mash_step.mosquito_rm <- function(module, bloodmeal_dt) {
+  past_kappa <- mosquito_rm_convert_bloodmeal(bloodmeal_dt)
+  step_output <- mosquito_rm_discrete_step(module, past_kappa)
   result <- list(
+    external_parameters = module$external_parameters,
     parameters = module$parameters,
-    state = today_state,
-    output = output
+    state = step_output$today_state,
+    output = step_output$output
   )
   class(result) <- "mosquito_rm"
   result
