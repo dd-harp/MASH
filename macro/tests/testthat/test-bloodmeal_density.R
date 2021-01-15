219ffda04b68bf173e54cfb7868ce38aa7d5f502
Drew Dolgert <adolgert@uw.edu>
2021-01-14 17:10:35 -0800

    amended calculation of dwell time to account for future time steps

--- macro/tests/testthat/test-bloodmeal_density.R
+++ macro/tests/testthat/test-bloodmeal_density.R
@@ -20,7 +20,8 @@ test_that("bloodmeal density assigns bites at location", {
   )
   health_dt <- sample_health_infection_status(params$human_cnt, params$duration)
   bite_weight <- runif(params$human_cnt, 0.2, 0.8)
-  movement_dt <- sample_move_location(params$human_cnt, params$location_cnt, params$duration)
+  movement_dt <- sample_move_location(
+      params$human_cnt, params$location_cnt, params$duration)
   mosquito_dt <- sample_mosquito_myz(params$location_cnt, params$duration)
 
   response = bld_bloodmeal_process(health_dt, movement_dt, mosquito_dt, params)
@@ -42,7 +43,8 @@ test_that("bloodmeal density processes sample data", {
   )
   health_dt <- sample_health_infection_status(params$human_cnt, params$duration)
   bite_weight <- runif(params$human_cnt, 0.2, 0.8)
-  movement_dt <- sample_move_location(params$human_cnt, params$location_cnt, params$duration)
+  movement_dt <- sample_move_location(
+      params$human_cnt, params$location_cnt, params$duration)
   mosquito_dt <- sample_mosquito_myz(params$location_cnt, params$duration)
 
   bloodmeal <- bloodmeal_density_module(params)
@@ -51,4 +53,46 @@ test_that("bloodmeal density processes sample data", {
   mosquito_infections <- infects_mosquito_path(bloodmeal)
   expect_equal(nrow(mosquito_infections), params$day_cnt * params$location_cnt)
   expect_equal(colnames(human_infections), c("times", "human"))
+})
+
+
+test_that("single_dwell translates events to dwell times", {
+  location_cnt <- 10L
+  step_duration <- 10.0
+  human_cnt <- 8L
+  place_cnt <- 5L
+  time_step <- 10.0
+  move_dt <- sample_move_location(human_cnt, place_cnt, time_step)
+  day_cnt <- as.integer(step_duration / 1)
+  move_cnt <- length(grep("Time", names(move_dt)))
+  h_idx <- 1
+  h_record <- move_dt[move_dt$ID == 7, ]
+  dwell.lt <- macro:::single_dwell(
+      h_record, location_cnt, move_cnt, 1, step_duration)
+  expect_true(all(colSums(dwell.lt) == 1))
+  expect_equal(dim(dwell.lt), c(location_cnt, day_cnt))
+})
+
+
+test_that("single_dwell handles times past first events", {
+  location_cnt <- 10L
+  step_duration <- 10.0
+  human_cnt <- 8L
+  place_cnt <- 5L
+  time_step <- 10.0
+  move_dt <- sample_move_location(human_cnt, place_cnt, time_step)
+  col_cnt <- length(colnames(move_dt))
+  time_cnt <- (col_cnt - 2) / 2
+  for (i in 1:time_cnt) {
+    colname <- paste0("Time", i)
+    move_dt[, colname] <- move_dt[, colname, with=FALSE] + 10
+  }
+  day_cnt <- as.integer(step_duration / 1)
+  move_cnt <- length(grep("Time", names(move_dt)))
+  h_idx <- 1
+  h_record <- move_dt[move_dt$ID == 7, ]
+  dwell.lt <- macro:::single_dwell(
+    h_record, location_cnt, move_cnt, 10, step_duration)
+  expect_true(all(colSums(dwell.lt) == 1))
+  expect_equal(dim(dwell.lt), c(location_cnt, day_cnt))
 })
