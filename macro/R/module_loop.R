87d93d856f663404bfc68bc7bb64bce9ddf5c920
Drew Dolgert <adolgert@uw.edu>
2021-01-20 17:01:12 -0800

    Made new observers to watch data flow.

--- macro/R/module_loop.R
+++ macro/R/module_loop.R
@@ -119,12 +119,12 @@ step_mainloop <- function(modules, observer, step_cnt = 1, dump_condition = NULL
   mosquito_trajectory <- mosquito_path(mosquito)
 
   for (step_idx in 1:step_cnt) {
-    observe_begin_step(observer, step_idx)
+    observer <- observe_begin_step(observer, step_idx)
 
     # Location depends on current health.
     location <- mash_step(location, health_path)
     human_path <- person_path(location)
-    observe_location(observer, human_path, step_idx)
+    observer <- observe_location(observer, human_path, step_idx)
 
     # Bloodmeal is the central piece where things come together.
     if (dump_condition("bloodmeal", step_idx)) dump_arguments(
@@ -134,21 +134,26 @@ step_mainloop <- function(modules, observer, step_cnt = 1, dump_condition = NULL
     bloodmeal <- mash_step(bloodmeal, health_path, human_path, mosquito_trajectory)
     human_bloodmeal_path <- infects_human_path(bloodmeal)
     mosquito_bloodmeal_path <- infects_mosquito_path(bloodmeal)
-    observe_bloodmeal_human(observer, human_bloodmeal_path, step_idx)
-    observe_bloodmeal_mosquito(observer, mosquito_bloodmeal_path, step_idx)
+    observer <- observe_bloodmeal_human(observer, human_bloodmeal_path, step_idx)
+    observer <- observe_bloodmeal_mosquito(observer, mosquito_bloodmeal_path, step_idx)
 
     # The mosquito moves us to the next step.
     mosquito <- mash_step(mosquito, mosquito_bloodmeal_path)
     mosquito_trajectory <- mosquito_path(mosquito)
-    observe_mosquito(mosquito, mosquito_trajectory, step_idx)
+    observer <- observe_mosquito(observer, mosquito_trajectory, step_idx)
 
     # Health moves us to the next step.
     health <- mash_step(health, human_bloodmeal_path)
     health_path <- human_disease_path(health)
-    observe_health(observer, health_path, step_idx)
+    observer <- observe_health(observer, health_path, step_idx)
 
-    observe_end_step(observer, step_idx)
+    observer <- observe_end_step(observer, step_idx)
   }
 
-  list(location = location, bloodmeal = bloodmeal, health = health, mosquito = mosquito)
+  modules$location <- location
+  modules$bloodmeal <- bloodmeal
+  modules$health <- health
+  modules$mosquito <- mosquito
+  modules$observer <- observer
+  modules
 }
