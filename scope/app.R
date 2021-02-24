60598215901d8a13495331cabeab93621be1613f
Drew Dolgert <adolgert@uw.edu>
2021-02-23 16:16:42 -0800

    can click the simulate button and run but no plots yet

--- scope/app.R
+++ scope/app.R
@@ -10,6 +10,143 @@
 library(shiny)
 library(shinythemes)
 library(reactable)
+library(macro)
+
+build_movement_module <- function(parameters, travel) {
+    lcnt <- parameters$location_cnt
+    start_at_home <- rep(1:parameters$location_cnt, parameters$humans.l)
+    stopifnot(length(start_at_home) == parameters$human_cnt)
+
+    stochastic_matrix <- matrix(
+        rep(travel, each = lcnt), nrow = lcnt, ncol = lcnt)
+    diag(stochastic_matrix) <- 0
+    # The rows should sum to one.
+    stochastic_matrix <- diag(1 / rowSums(stochastic_matrix)) %*%
+        stochastic_matrix
+    return_matrix <- matrix(
+        1, nrow = parameters$location_cnt, ncol = parameters$location_cnt)
+    diag(return_matrix) <- 0
+    return_matrix <- diag(1 / rowSums(return_matrix)) %*% return_matrix
+
+    simpletrip_parameters <- list2env(list(
+        trip_rate = rep(0.1, parameters$location_cnt),
+        trip_dest = stochastic_matrix,
+        return_home_rate = return_matrix,
+        npatch = parameters$location_cnt,
+        home = start_at_home,
+        current = start_at_home,
+        duration_days = parameters$duration_days
+    ))
+    simple_trip_module(simpletrip_parameters)
+}
+
+
+build_health_module <- function(parameters, pfpr) {
+    health_parameters <- list(
+        recovery_rate = 1 / 200,
+        people_cnt = parameters$human_cnt,
+        duration_days = parameters$duration_days,
+        initial_pfpr = pfpr[1]  # XXX want to set by location!
+    )
+    human_si_module(health_parameters)
+}
+
+
+build_mosquito_module <- function(parameters, M, Z) {
+    year_days <- 365
+    # XXX M should determine lambda.
+    mosquito_parameters <- list(
+        duration = parameters$duration_days,
+        N = parameters$location_cnt,
+        lambda = matrix(
+            rep(100, year_days * parameters$location_cnt),
+            nrow = parameters$location_cnt),
+        psi = diag(parameters$location_cnt),  # diffusion matrix
+        biting_weight = 0.4,  # The human biting weight. Should be from human.XXX
+        EIP = rep(12, year_days),  # Extrinsic incubation period,
+        maxEIP = 12,  # Maximum length of EIP.
+        p = 0.9,  # survival
+        # human blood feeding rate, the proportion of mosquitoes that feed on humans each day
+        a = 0.6,
+        infected_fraction = Z / M,
+        year_day_start = 1
+    )
+    mosquito_rm_module(mosquito_parameters)
+}
+
+
+build_bloodmeal_module <- function(parameters) {
+    params <- list(
+        human_cnt = parameters$human_cnt,
+        location_cnt = parameters$location_cnt,
+        duration = parameters$duration_days,
+        day_duration = 1,
+        dispersion = 1.5,
+        day_cnt = 10,
+        day_start = 1,
+        biting_weight = 0.5
+    )
+    bloodmeal_density_module(params)
+}
+
+
+build_modules <- function(human, pfpr, travel, M, Z) {
+    parameters <- list(
+        human_cnt = sum(human),
+        humans.l = human,
+        location_cnt = length(travel),
+        duration_days = 10
+    )
+    movement <- build_movement_module(parameters, travel)
+    health <- build_health_module(parameters, pfpr)
+    mosquito <- build_mosquito_module(parameters, M, Z)
+    bloodmeal <- build_bloodmeal_module(parameters)
+    list(location = movement,
+         health = health,
+         mosquito = mosquito,
+         bloodmeal = bloodmeal)
+}
+
+
+#' Run a MASH simulation.
+#' @param human a vector of the number of humans at each location.
+#' @param pfpr The PfPR of humans at each location.
+#' @param travel Rate of travel to other locations.
+#' @param M total number of msoquitoes at each location.
+#' @param Z number of infectious mosquitoes at each location.
+generate_data <- function(human, pfpr, travel, M, Z) {
+    modules <- build_modules(human, pfpr, travel, M, Z)
+
+    step_cnt <- 5
+    observer <- complete_observer()
+    singleobs <- vector(mode = "list", length = step_cnt)
+    msgnames <- c(
+        "location", "bloodmeal_human", "bloodmeal_mosquito", "health", "mosquito")
+    dump_if <- function(step, idx) { step == "bloodmeal" }
+    seen <- list()
+    for (main_idx in 1:step_cnt) {
+        cat(paste("Running 10-day step", main_idx, "\n"))
+        modules <- step_mainloop(modules, observer, main_idx, dump_if)
+        singleobs[[main_idx]] <- lapply(
+            msgnames,
+            function(msgtype) {
+                data.table::rbindlist(modules$observer[[msgtype]])
+            }
+        )
+        names(singleobs[[main_idx]]) <- msgnames
+    }
+    observations <- lapply(
+        msgnames,
+        function(msgname) {
+            data.table::rbindlist(
+                lapply(singleobs, function(obs) obs[[msgname]])
+            )
+        }
+    )
+    names(observations) <- msgnames
+    observations
+}
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -28,9 +165,9 @@ ui <- fluidPage(
         column(
             2,
             "PfPR",
-            numericInput("n1", label="n1", value = 10, width = "80px"),
-            numericInput("n2", label="n2", value = 5, width = "80px"),
-            numericInput("n3", label="n3", value = 15, width = "80px")
+            numericInput("pfpr1", label="pfpr1", value = 0.2, width = "80px"),
+            numericInput("pfpr2", label="pfpr2", value = 0.2, width = "80px"),
+            numericInput("pfpr3", label="pfpr3", value = 0.2, width = "80px")
         ),
         column(
             4,
@@ -49,9 +186,9 @@ ui <- fluidPage(
         column(
             2,
             "Z",
-            numericInput("mosy1", label="mosy1", value = 20),
-            numericInput("mosy2", label="mosy2", value = 30),
-            numericInput("mosy3", label="mosy3", value = 10)
+            numericInput("z1", label="z1", value = 20),
+            numericInput("z2", label="z2", value = 30),
+            numericInput("z3", label="z3", value = 10)
         )
     ),
     fluidRow(
@@ -60,13 +197,23 @@ ui <- fluidPage(
     )
 )
 
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    mosy_init <- eventReactive(input$simulate, { input$mosy1 })
+    observations <- eventReactive(input$simulate, {
+        human <- c(input$n1, input$n2, input$n3)
+        pfpr <- c(input$pfpr1, input$pfpr2, input$pfpr3)
+        travel <- c(input$travel1, input$travel2, input$travel3)
+        m <- c(input$mosy1, input$mosy2, input$mosy3)
+        z <- c(input$z1, input$z2, input$z3)
+        generate_data(human, pfpr, travel, m, z)
+        })
     output$greeting <- renderText({
-        paste0("Hello ", mosy_init())
+        obs <- observations()
+        paste0("Hello ", length(obs$bloodmeal_mosquito))
     })
 }
+
 
 # Run the application
 shinyApp(ui = ui, server = server)
